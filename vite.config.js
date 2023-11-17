4dff4d31d3289d00226da55a897b573813c7315d
lucasch98 <lucasch98.lch@gmail.com>
2023-11-16 21:03:33 -0300

    fix erros pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import {VitePWA} from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
+const manifestForPlugin = {
+	registerType: "prompt",
+	includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+	manifest: {
+		name: "Weather Ups",
+		short_name: "Weathe Ups",
+		description: "An app that can show weather forecast for your city.",
+		icons: [
+			{
+				src: "/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "any maskable",
+			},
+			{
+				src: "/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "any maskable",
+			},
+			{
+				src: "/apple-touch-icon.png",
+				sizes: "180x180",
+				type: "image/png",
+				purpose: "any maskable",
+			},
+			{
+				src: "/maskable_icon.png",
+				sizes: "225x225",
+				type: "image/png",
+				purpose: "any maskable",
+			},
+		],
+		theme_color: "#317EFB",
+		background_color: "#e8ebf2",
+		display: "standalone",
+		scope: "/",
+		start_url: "/",
+		orientation: "portrait",
+	},
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
-})
+	base: "./",
+	plugins: [react(), VitePWA(manifestForPlugin)],
+});
